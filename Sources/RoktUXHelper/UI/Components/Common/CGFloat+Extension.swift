0ee7b1256022b0ae4e3ffbe0b29544df53faee6e
Wei Yew Teoh <wei.yew.teoh@rokt.com>
2025-02-14 13:00:31 +1100

    NI-558 - fix linting

--- Sources/RoktUXHelper/UI/Components/Common/CGFloat+Extension.swift
+++ Sources/RoktUXHelper/UI/Components/Common/CGFloat+Extension.swift
@@ -13,6 +13,6 @@ import Foundation
 
 extension CGFloat {
     func precised(_ value: Int = 2) -> CGFloat {
-        (self * pow(10.0, CGFloat(value))).rounded()/pow(10.0,CGFloat(value))
+        (self * pow(10.0, CGFloat(value))).rounded()/pow(10.0, CGFloat(value))
     }
 }
