51e70158e0637160a0f386e440e274c1ccbbc8f3
Wei Yew Teoh <wei.yew.teoh@rokt.com>
2025-04-04 10:17:17 +1100

    update view inspector with xcode16.3

--- Tests/RoktUXHelperTests/UI/Components/TestAccessibilityGroupedModelInColumnComponent.swift
+++ Tests/RoktUXHelperTests/UI/Components/TestAccessibilityGroupedModelInColumnComponent.swift
@@ -17,98 +17,7 @@ import DcuiSchema
 
 @available(iOS 15.0, *)
 final class TestAccessibilityGroupedModelInColumnComponent: XCTestCase {
-    
-#if compiler(>=6)
-    func test_column() throws {
-        let model = try get_model()
-        
-        guard case .column(let columnUIModel) = model else {
-            XCTFail()
-            return
-        }
-        
-        let view = TestPlaceHolder(layout: LayoutSchemaViewModel.column(columnUIModel))
-        
-        let sut = try view.inspect()
-            .find(TestPlaceHolder.self)
-            .find(EmbeddedComponent.self)
-            .find(ViewType.VStack.self)[0]
-            .find(LayoutSchemaComponent.self)
-            .find(ColumnComponent.self)
-        
-        // test custom modifier class
-        
-        let modifierContent = try sut
-            .modifierIgnoreAny(LayoutSchemaModifier.self)
-            .ignoreAny(ViewType.ViewModifierContent.self)
-        
-        let paddingModifier = try modifierContent.modifier(PaddingModifier.self).actualView().padding
-        // test the effect of custom modifier
-        XCTAssertEqual(
-            paddingModifier,
-            FrameAlignmentProperty(top: 18, right: 24, bottom: 0, left: 24)
-        )
-    
-        // Test weight = 1 add maxHeight .infinity
-        XCTAssertEqual(try modifierContent.flexFrame().maxHeight, .infinity)
-        
-        // background
-        let backgroundModifier = try modifierContent.modifier((BackgroundModifier.self))
-        let backgroundStyle = try backgroundModifier.actualView().backgroundStyle
-        
-        XCTAssertEqual(backgroundStyle?.backgroundColor, ThemeColor(light: "#F5C1C4", dark: "#F5C1C4"))
-        
-        // border
-        let borderModifier = try modifierContent.modifier((BorderModifier.self))
-        let borderStyle = try borderModifier.actualView().borderStyle
-        
-        XCTAssertNil(borderStyle)
-        
-        // alignment
-        let alignment = try sut
-            .find(ViewType.VStack.self)
-            .alignment()
-        XCTAssertEqual(alignment, .center)
-    }
-    
-    func test_columnComponent_computedProperties_accessibility() throws {
-        let model = try get_model()
-        
-        guard case .column(let columnUIModel) = model else {
-            XCTFail()
-            return
-        }
-        
-        let view = TestPlaceHolder(layout: LayoutSchemaViewModel.column(columnUIModel))
-        
-        let sut = try view.inspect()
-            .view(TestPlaceHolder.self)
-            .find(EmbeddedComponent.self)
-            .find(ViewType.VStack.self)[0]
-            .find(LayoutSchemaComponent.self)
-            .find(ColumnComponent.self)
-            .actualView()
-        
-        let defaultStyle = sut.model.defaultStyle?[0]
-        
-        XCTAssertEqual(sut.style, defaultStyle)
-        
-        XCTAssertEqual(sut.containerStyle, defaultStyle?.container)
-        XCTAssertEqual(sut.dimensionStyle, defaultStyle?.dimension)
-        XCTAssertEqual(sut.flexStyle, defaultStyle?.flexChild)
-        XCTAssertEqual(sut.backgroundStyle, defaultStyle?.background)
-        XCTAssertEqual(sut.spacingStyle, defaultStyle?.spacing)
-        XCTAssertEqual(sut.borderStyle, defaultStyle?.border)
-        
-        XCTAssertEqual(sut.passableBackgroundStyle, defaultStyle?.background)
-        
-        XCTAssertEqual(sut.verticalAlignment, .center)
-        XCTAssertEqual(sut.horizontalAlignment, .center)
-        
-        XCTAssertEqual(sut.accessibilityBehavior, .combine)
-        
-    }
-#else
+
     func test_column() throws {
         let model = try get_model()
         
@@ -193,7 +102,7 @@ final class TestAccessibilityGroupedModelInColumnComponent: XCTestCase {
         XCTAssertEqual(sut.accessibilityBehavior, .combine)
         
     }
-#endif
+
     func get_model() throws -> LayoutSchemaViewModel {
         let transformer = LayoutTransformer(layoutPlugin: get_mock_layout_plugin())
         let accessibilityGroup = ModelTestData.ColumnData.accessibilityGroupedColumn()
