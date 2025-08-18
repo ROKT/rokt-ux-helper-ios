0b286a4d5f3d59acd350750e0894c413d0d9f89c
Thomson Thomas <thomson.thomas@rokt.com>
2025-08-18 12:43:42 +1000

    Fix unit tests

--- Tests/RoktUXHelperTests/UI/Components/Distribution/TestCarouselDistributionComponent.swift
+++ Tests/RoktUXHelperTests/UI/Components/Distribution/TestCarouselDistributionComponent.swift
@@ -36,19 +36,19 @@ final class TestCarouselDistributionComponent: XCTestCase {
             .view(CarouselDistributionComponent.self)
             .actualView()
 
-        let carousel = try carouselComponent
-            .inspect()
-            .find(LayoutSchemaComponent.self)
+        let geometryReader = try carouselComponent.inspect().geometryReader()
 
         // test custom modifier class
-        let paddingModifier = try carousel.modifier(PaddingModifier.self)
+        let paddingModifier = try geometryReader.modifier(PaddingModifier.self)
         XCTAssertEqual(try paddingModifier.actualView().padding, FrameAlignmentProperty(top: 3, right: 4, bottom: 5, left: 6))
 
         // test the effect of custom modifier
-        let padding = try carousel.padding()
+        let padding = try geometryReader.padding()
         XCTAssertEqual(padding, EdgeInsets(top: 3.0, leading: 6.0, bottom: 5.0, trailing: 4.0))
 
-        XCTAssertEqual(try carousel.accessibilityLabel().string(), "Page 1 of 1")
+        // Test accessibility label on the carousel item (LayoutSchemaComponent)
+        let carouselItem = try geometryReader.find(LayoutSchemaComponent.self)
+        XCTAssertEqual(try carouselItem.accessibilityLabel().string(), "Page 1 of 1")
 
         carouselComponent.model.goToNextOffer(nil)
         XCTAssertTrue(closeActionCalled)
