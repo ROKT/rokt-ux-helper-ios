1a5e8e1bc091d9d2ce4a687427114ac6cf8349c6
James Newman <james.newman@rokt.com>
2025-09-02 15:50:38 +1000

    Add temp sample view

--- Example/Example/SampleView.swift
+++ Example/Example/SampleView.swift
@@ -27,27 +27,32 @@ struct SampleView: View {
     }
 
     var body: some View {
-        RoktLayoutView(
-            experienceResponse: vm.experienceResponse,
-            location: "#target_element", // "targetElementSelector" in experience JSON file
-            config: RoktUXConfig.Builder().colorMode(.system).imageLoader(vm).build()
-        ) { uxEvent in
-            if uxEvent is RoktUXEvent.LayoutCompleted {
-                dismiss()
-            } else if uxEvent is RoktUXEvent.LayoutFailure {
-                onLayoutFailure?()
-                dismiss()
-            } else if let uxEvent = (uxEvent as? RoktUXEvent.OpenUrl) {
-                // Handle open URL event
-                vm.handleURL(uxEvent)
-            }
-            // Handle UX events here
+        ZStack {
+            Image("Map")
+                .scaledToFit()
 
-        } onPlatformEvent: { _ in
-            // Send these platform events to Rokt API
-        }.sheet(item: $vm.urlToOpen) {
-            SafariWebView(url: $0)
+            RoktLayoutView(
+                experienceResponse: vm.experienceResponse,
+                location: "#target_element", // "targetElementSelector" in experience JSON file
+                config: RoktUXConfig.Builder().colorMode(.system).imageLoader(vm).build()
+            ) { uxEvent in
+                if uxEvent is RoktUXEvent.LayoutCompleted {
+                    dismiss()
+                } else if uxEvent is RoktUXEvent.LayoutFailure {
+                    onLayoutFailure?()
+                    dismiss()
+                } else if let uxEvent = (uxEvent as? RoktUXEvent.OpenUrl) {
+                    // Handle open URL event
+                    vm.handleURL(uxEvent)
+                }
+                // Handle UX events here
+            } onPlatformEvent: { _ in
+                // Send these platform events to Rokt API
+            }.sheet(item: $vm.urlToOpen) {
+                SafariWebView(url: $0)
+            }
         }
+        .ignoresSafeArea()
     }
 }
 
