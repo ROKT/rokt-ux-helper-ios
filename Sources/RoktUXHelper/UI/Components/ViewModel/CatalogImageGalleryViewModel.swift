7d2450c8ef793f43f21f2b08be4f74b31af64438
Thomson Thomas <thomson.thomas@rokt.com>
2025-10-14 13:56:49 -0400

    Update license comments

--- Sources/RoktUXHelper/UI/Components/ViewModel/CatalogImageGalleryViewModel.swift
+++ Sources/RoktUXHelper/UI/Components/ViewModel/CatalogImageGalleryViewModel.swift
@@ -8,7 +8,6 @@
 //  You may not use this file except in compliance with the License.
 //
 //  You may obtain a copy of the License at https://rokt.com/sdk-license-2-0/
-//
 
 import Combine
 import Foundation
