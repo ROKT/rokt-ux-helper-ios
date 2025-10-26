ce231c50710ebf5bdd0e24373df8f83f086ad846
Thomson Thomas <thomson.thomas@rokt.com>
2025-10-25 21:22:20 -0400

    fix: stack overflow issue on device
    Fix the stack overflow and EXC_BAD_ACCESS error when complex nested DCUI JSON
    Create custom thread for deserializing the JSON and increase the stacksize of the thread to 8MB
    Keep the same quality of service as the current thread

--- Sources/RoktUXHelper/Services/Decoder/RoktDecoder.swift
+++ Sources/RoktUXHelper/Services/Decoder/RoktDecoder.swift
@@ -15,10 +15,31 @@ import Foundation
 struct RoktDecoder {
 
     func decode<T: Decodable>(_ type: T.Type, _ string: String) throws -> T {
-        try string.data(using: .utf8)
-            .flatMap {
-                try JSONDecoder().decode(type, from: $0)
+        guard let data = string.data(using: .utf8) else {
+            throw RoktUXError.experienceResponseMapping
+        }
+
+        var result: Result<T, Error>?
+        let semaphore = DispatchSemaphore(value: 0)
+
+        let decodingThread = Thread {
+            defer { semaphore.signal() }
+            do {
+                let decoded = try JSONDecoder().decode(type, from: data)
+                result = .success(decoded)
+            } catch {
+                result = .failure(error)
             }
+        }
+        decodingThread.name = "com.rokt.decoder"
+        decodingThread.stackSize = max(decodingThread.stackSize, 8 * 1024 * 1024)
+        decodingThread.qualityOfService = Thread.current.qualityOfService
+        decodingThread.start()
+
+        semaphore.wait()
+
+        return try result
             .unwrap(orThrow: RoktUXError.experienceResponseMapping)
+            .get()
     }
 }
