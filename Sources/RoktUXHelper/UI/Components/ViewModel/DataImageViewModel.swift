9190a95ce9df835119384e3504442fbc63326f71
Aryan Bhatia <53007410+aryanbh11@users.noreply.github.com>
2026-09-04 09:36:50 -0400

    refactor(catalog): remove unused view model storage

--- Sources/RoktUXHelper/UI/Components/ViewModel/DataImageViewModel.swift
+++ Sources/RoktUXHelper/UI/Components/ViewModel/DataImageViewModel.swift
@@ -6,7 +6,6 @@ class DataImageViewModel: Hashable, Identifiable, ObservableObject, ScreenSizeAd
     let id: UUID = UUID()
 
     let image: CreativeImage?
-    let catalogItemContext: CatalogItemContext?
     let defaultStyle: [DataImageStyles]?
     let pressedStyle: [DataImageStyles]?
     let hoveredStyle: [DataImageStyles]?
@@ -21,10 +20,8 @@ class DataImageViewModel: Hashable, Identifiable, ObservableObject, ScreenSizeAd
          pressedStyle: [DataImageStyles]?,
          hoveredStyle: [DataImageStyles]?,
          disabledStyle: [DataImageStyles]?,
-         layoutState: (any LayoutStateRepresenting)?,
-         catalogItemContext: CatalogItemContext? = nil) {
+         layoutState: (any LayoutStateRepresenting)?) {
         self.image = image
-        self.catalogItemContext = catalogItemContext
         self.defaultStyle = defaultStyle
         self.pressedStyle = pressedStyle
         self.hoveredStyle = hoveredStyle
